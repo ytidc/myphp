8b4993f9a01db014da26bf866b663a17831980cc
yy tth <755048652@qq.com>
2022-06-24 23:51:54 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,5 +4,9 @@ error_reporting(E_ERROR);ini_set("display_errors","Off");
 switch ($_GET['t']){
   case '阅读':
     include('阅读.php');
+    barek;
+  case 'jx':
+    include('jiexi.php');
+    barek;
 }
 ?>
